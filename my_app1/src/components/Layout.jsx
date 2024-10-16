b4814a47e41c287b184d6a8aaab99d9c92a3527c
karthik40404 <karpython123@gmail.com>
2024-10-16 21:30:19 +0530

    react

--- my_app1/src/components/Layout.jsx
+++ my_app1/src/components/Layout.jsx
@@ -17,12 +17,10 @@
 // export default Layout
 
 import { Outlet, Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 function Layout() {
   return (
     <>
-      <nav className=" navbar navbar-expand-lg navbar-dark bg-primary" >
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary" >
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">MyApp</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -43,11 +41,8 @@ function Layout() {
           </div>
         </div>
       </nav>
-      <div className="container">
         <Outlet />
-      </div>
     </>
   );
 }
-
 export default Layout;
